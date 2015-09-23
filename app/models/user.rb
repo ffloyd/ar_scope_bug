fe1378acadbea436ae11a44b0ae47b5cec437dc0
Roman Kolesnev <rvkolesnev@gmail.com>
2015-09-23 23:18:57 +0300

    Add useless_join scope and some tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   scope :with_role, lambda { |role_name|
     joins(:roles).where(roles: { name: role_name })
   }
+
+  scope :useless_join, lambda {
+    joins("LEFT OUTER JOIN (SELECT 'Aaron'::text AS name) subquery ON subquery.name = users.first_name")
+  }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "with_role('tenderlover') returns only Aaron" do
+  test "User.with_role('tenderlover') returns only Aaron" do
     assert_equal User.with_role('tenderlover'), [users(:aaron)]
+  end
+
+  test 'User.useless_join scope does nothing' do
+    assert_equal User.useless_join.to_a, User.all.to_a
+  end
+
+  test "User.useless_join.with_role('tenderlover') returns only Aaron" do
+    assert_equal User.useless_join.with_role('tenderlover'), [users(:aaron)]
   end
 end
