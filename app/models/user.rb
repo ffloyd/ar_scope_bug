23c6707936e603d9409855ea74f945b6240e1bae
Roman Kolesnev <rvkolesnev@gmail.com>
2015-09-23 23:03:45 +0300

    Add UsersRole model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name
+
+  has_many :users_roles, dependent: :destroy
+  has_many :roles, -> { distinct }, through: :users_roles
+
+  scope :with_role, lambda { |role_name|
+    joins(:roles).where(roles: { name: role_name })
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923195353) do
+ActiveRecord::Schema.define(version: 20150923195601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,16 @@ ActiveRecord::Schema.define(version: 20150923195353) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users_roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users_roles", ["role_id"], name: "index_users_roles_on_role_id", using: :btree
+  add_index "users_roles", ["user_id"], name: "index_users_roles_on_user_id", using: :btree
+
+  add_foreign_key "users_roles", "roles"
+  add_foreign_key "users_roles", "users"
 end
