c486781c3e51956357fb5f112fb29d9697206499
Roman Kolesnev <rvkolesnev@gmail.com>
2015-09-23 23:41:09 +0300

    Rewrite tests to demonstrate bug

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "User.with_role('tenderlover') returns only Aaron" do
+  test 'scope #with_role works' do
     assert_equal User.with_role('tenderlover'), [users(:aaron)]
   end
 
-  test 'User.useless_join scope does nothing' do
+  test 'scope #useless_join works' do
     assert_equal User.useless_join.to_a, User.all.to_a
   end
 
-  test "User.useless_join.with_role('tenderlover') returns only Aaron" do
+  test 'scope chain "useless_join -> with_role" works' do
     assert_equal User.useless_join.with_role('tenderlover'), [users(:aaron)]
+  end
+
+  # there bug revealed
+  test 'scope chain "with_role -> useless_join" works' do
+    assert_equal User.with_role('tenderlover').useless_join, [users(:aaron)]
+  end
+
+  # but there is no bug Oo
+  test 'scope #with_role works with complex join from #useless_join when it constructed outside model' do
+    subquery_sql = User.select('users.id AS user_id').to_sql
+    subject = User
+              .with_role('tenderlover')
+              .joins("INNER JOIN (#{subquery_sql}) subquery ON subquery.user_id = users.id")
+    assert_equal subject, [users(:aaron)]
   end
 end
